302b539228f6fda17fb7385ccd7efc9a2317c1d2
AlexKazDrumm <alexkazdrumm@gmail.com>
2025-12-14 21:21:11 +0500

    upd

--- src/data/game-sprites.js
+++ src/data/game-sprites.js
@@ -106,7 +106,7 @@
     // Автарки автостопщиков по id (используются и в дороге, и в HUD)
     hitchhiker_s1_h1: "assets/avatars/hitchhikers/s1_h1.png",
     hitchhiker_s1_h2: "assets/avatars/hitchhikers/s1_h2.png",
-    hitchhiker_s1_h3: "assets/avatars/hitchhikers/s1_h3.png.png",
+    hitchhiker_s1_h3: "assets/avatars/hitchhikers/s1_h3.png",
     hitchhiker_s1_h4: "assets/avatars/hitchhikers/s1_h4.png",
     hitchhiker_s1_h5: "assets/avatars/hitchhikers/s1_h5.png",
     hitchhiker_s2_h1: "assets/avatars/hitchhikers/s2_h1.png",
